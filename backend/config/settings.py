7da02c955a8aa3e372d113b2aec712c275358999
Jeremiah Pantaras <jeremiahpantaras@gmail.com>
2026-03-23 10:09:59 +0800

    new:deploy

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -102,6 +102,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://malasakit-web.vercel.app",
+    "https://malasakit.com",
 ]
 
 
